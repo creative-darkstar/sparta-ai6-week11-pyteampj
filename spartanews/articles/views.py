a3c548967b8bd16e6d1d55300bed6b13960c9e95
creative.darkstar <creative.darkstar@gmail.com>
2024-05-07 17:11:15 +0900

    Edit: trial of adding filter and ordering of article-list

--- spartanews/articles/views.py
+++ spartanews/articles/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, generics, filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import ContentInfo, CommentInfo
 from .serializers import ArticleSerializer
+
+
+# class ArticleAPIView(generics.ListAPIView):
+#     serializer_class = ArticleSerializer
+#     filter_backends = [filters.OrderingFilter]
+#     # ordering_fields = [
+#     #     "article_point",
+#     #     "-create_dt",
+#     # ]
+#     ordering = [
+#         "article_point",
+#         "-create_dt",
+#     ]
+#
+#     def get_queryset(self):
+#         query_params = self.request.query_params
+#         order_by = query_params.get("order-by")
+#         user = query_params.get("user")
+#         liked_by = query_params.get("liked-by")
+#         favorite_by = query_params.get("favorite-by")
+#
+#         if order_by == "new":
+#             self.ordering.remove("article_point")
+#             print(self.ordering)
+#         else:
+#             self.ordering.remove("-create_dt")
+#             print(self.ordering)
+#
+#         return ContentInfo.objects.all()
 
 
 class ArticleAPIView(APIView):
