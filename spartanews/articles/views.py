29da39832efcc49d4b5e1e3b4a336ba968387bf2
creative.darkstar <creative.darkstar@gmail.com>
2024-05-07 15:03:08 +0900

    Edit: removed useless modules of articles app's views.py

--- spartanews/articles/views.py
+++ spartanews/articles/views.py
@@ -3,19 +3,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# from datetime import datetime, timedelta
-
 from .models import ContentInfo, CommentInfo
 from .serializers import ArticleSerializer
-
-
-# def article_point(dt, cm_cnt, like_cnt):
-#     # dt: create_dt
-#     # cm_cnt: comments count
-#     # like_cnt: article(content) likes count
-#     now = datetime.now()
-#
-#     return -5 * (now - dt).days + 3 * cm_cnt + like_cnt
 
 
 class ArticleAPIView(APIView):
