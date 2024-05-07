d02b0c2e91182d0a9736e1e85217a6d4025f556b
creative.darkstar <creative.darkstar@gmail.com>
2024-05-07 14:56:37 +0900

    Add: add 'article-all' api and func

--- spartanews/articles/urls.py
+++ spartanews/articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import ArticleAPIView
 
 app_name = 'articles'
 
 urlpatterns = [
-
+    path("", ArticleAPIView.as_view(), name='article_all'),
 ]
--- spartanews/articles/views.py
+++ spartanews/articles/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+# from datetime import datetime, timedelta
+
+from .models import ContentInfo, CommentInfo
+from .serializers import ArticleSerializer
+
+
+# def article_point(dt, cm_cnt, like_cnt):
+#     # dt: create_dt
+#     # cm_cnt: comments count
+#     # like_cnt: article(content) likes count
+#     now = datetime.now()
+#
+#     return -5 * (now - dt).days + 3 * cm_cnt + like_cnt
+
+
+class ArticleAPIView(APIView):
+
+    def get(self, request):
+        rows = ContentInfo.objects.filter(is_visible=True)
+        serializer = ArticleSerializer(rows, many=True)
+        return Response(serializer.data)
