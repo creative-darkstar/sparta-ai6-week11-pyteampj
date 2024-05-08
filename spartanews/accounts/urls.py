597ab98f9b4c919a52cb6de44a1211395217a575
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-05-08 15:29:00 +0900

    로그아웃 기능 구현

--- spartanews/accounts/urls.py
+++ spartanews/accounts/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("signup/", views.SignupAPIView.as_view(), name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", TokenBlacklistView.as_view(), name="logout"), # access, refresh 토큰이 있는 경우 로그인 된 상태. 이를 없애줌으로써 로그아웃.
 ]
