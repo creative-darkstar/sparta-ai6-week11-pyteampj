293700ec23fcd839028a5c395dc00251577d289d
Jenhynn <159970972+Jenhynn@users.noreply.github.com>
2024-05-09 15:41:15 +0900

    ManyToMany 글 관련 serializers.py에 추가

--- spartanews/accounts/serializers.py
+++ spartanews/accounts/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
+    # ManyToMany
+    contents = ContentSerializer(read_only = True, many=True)
+
     class Meta:
         model = get_user_model()
         fields = [
